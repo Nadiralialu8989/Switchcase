2a1fa7cd2af2cebaece7e1763cc875bb0783d65a
Nadiralialu8989 <160329916+Nadiralialu8989@users.noreply.github.com>
2024-03-29 00:12:27 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,54 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+// Step 1
+
+const answers:{
+    numberOne: number,
+    numberTwo: number,
+    operator: string,
+} = await inquirer.prompt([
+    {type: "number",
+    name: "numberOne",
+  message: "Enter your first number"
+},
+    {type: "number",
+    name: "numberTwo",
+    message: "Enter your second number"
+  },
+  {
+    type: "list",
+    name: "operator",
+    message: "Enter your operator",
+    choices: ["+","-","*","/"]}
+])
+
+
+// Step 2
+
+
+const { numberOne, numberTwo, operator } = answers;
+
+
+let result
+  switch (operator) {
+  case "+":
+   result = numberOne + numberTwo;
+  break;
+  case "-":
+    result = numberOne - numberTwo;
+  break;
+  case "*":
+  result = numberOne * numberTwo;
+  break;
+  case "/":
+    result = numberOne / numberTwo;
+  break;
+  
+  default: 
+  console.log("Invalid Operator");
+  }
+console.log(chalk.green(`${numberOne} ${operator} ${numberTwo} = ${result}`));
+
